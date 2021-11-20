d8a5ae9df6584e27c83162b3836bf74938e495fb
Prerna Chitransh <chitranshcse@gmail.com>
2021-11-20 23:05:38 +0530

    currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'converter';
+  result ="";
+  currency = "";
+  value = 0;
+  ans =0;
+  onSubmit()
+  {
+    if(this.currency=="INR")
+    {
+      this.ans = this.value / 74.24;
+      
+      this.result = "Dollar Equivalent is " + this.ans;
+     
+    }
+    else if(this.currency=="Dollar")
+    {
+      this.ans = this.value * 74.24;
+      this.result = "INR Equivalent is "+this.ans;
+    }
+    else
+    {
+      this.result = "Invalid Input";
+    }
+
+  }
+
 }
